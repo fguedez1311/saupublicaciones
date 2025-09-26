b6c238b91a8cc60ee9a837feb58035b7856df559
fguedez1311 <46085315+fguedez1311@users.noreply.github.com.>
2025-09-26 15:15:23 -0400

    Se actualizó la data de las publicaciones

--- src/data/deltaData.ts
+++ src/data/deltaData.ts
@@ -5,8 +5,99 @@ export const publicacionesData:IDelta[] = [
                 "id":1,
                 "title": "Mapa Temático",
                 "description": "Contiene las ubicaciones geográfica de las instituciones del municipio Tucupita",
-                "format": "html",
+                "format": "HTML",
                 "url": "https://sites.google.com/view/mapainstitucionesdeldelta/p%C3%A1gina-principal?authuser=5"
+            },
+             {
+                "id":2,
+                "title": "Gráfico de Pirámide de Población",
+                "description": "Piramide de población, seguún grupo de edades. Fuente Censo de población y vivienda 2011",
+                "format": "PDF",
+                "url": "https://drive.google.com/file/d/1HAk5MEv-PF-gZrozMbSMe0DhWvFKEV1S/view?usp=drive_link"
+            },
+            {
+                "id":3,
+                "title": "Tabla Densidad Poblacional",
+                "description": "Densidad Poblacional, según Municipios y Parroquias, 2025. Proyecciones al 30 de junio de cada año, calculada en el trimestre del año 2013, con base al censo 2011",
+                "format": "PDF",
+                "url": "https://drive.google.com/file/d/1XFJE-YsNRAU7GedhDYZ703BY3CJB-sXZ/view?usp=drive_link"
+            },
+            {
+                "id":4,
+                "title": "Tabla de Proyección de Población por Municipio, 2025-2025",
+                "description": "Proyección de Población por Municipio, 2025-2025.: Proyecciones al 30 de junio de cada año, calculadas en el segundo trimestre del año 2013, con base al Censo 2011  ",
+                "format": "PDF",
+                "url": "https://drive.google.com/file/d/1fs1mu8Ozve8DO2v7mY9tvYJnf-2lhY62/view?usp=drive_link"
+            },
+            {
+                "id":5,
+                "title": "Tabla de Personas con Discapacidad, por Municipios y parroquias",
+                "description": "Personas con Discapacidad, por Municipios y Parroquias, 2025. Fuente Concejo Nacional  de Personas con Discapacidad (CONAPDIS). Procesado por Instituto Nacional de Estadística(INE)",
+                "format": "PDF",
+                "url": "https://drive.google.com/file/d/1hqGtwJG0LIaq_ykWsZP0PHCrJ5RxPupW/view?usp=drive_link"
+            },
+            {
+                "id":6,
+                "title": "Tabla de Población, según grupos de edad y sexo, 2020-2025",
+                "description": "Población, según grupos de edad y sexo, 2020-2025. Proyecciones al 30 de junio de cada año, calculadas en el segundo trimestre del año 2013, con base al Censo 2011.",
+                "format": "PDF",
+                "url": "https://drive.google.com/file/d/1rxojGepR39ZqfF8AmtCd4yDYab8IWj9Q/view?usp=drive_link"
+            },
+            {
+                "id":7,
+                "title": "Municipio Antonio Diaz, Piramide de población según grupo de edades, 2025",
+                "description": "Proyecciones basadas en el Censo de Población y Vivienda 2011",
+                "format": "PDF",
+                "url": "https://drive.google.com/file/d/1pwcpx65RoCJSBXG-js4dmpaAe5hsNeZ8/view?usp=drive_link"
+            },
+            {
+                "id":8,
+                "title": "Municipio Antonio Díaz, Población, según grupos de edad y sexo, 2020-2025",
+                "description": "Proyecciones al 30 de junio de cada año, calculadas en el segundo trimestre del año 2013, con base al Censo 2011",
+                "format": "PDF",
+                "url": "https://drive.google.com/file/d/1_7IUOntYetFtyQjxN4UuTqmb4tdSDHni/view?usp=drive_link"
+            },
+            {
+                "id":9,
+                "title": "Municipio Casacoima, Población, según grupos de edad y sexo, 2020-2025",
+                "description": "Proyecciones al 30 de junio de cada año, calculadas en el segundo trimestre del año 2013, con base al Censo 2011.",
+                "format": "PDF",
+                "url": "https://drive.google.com/file/d/11Zz-m7muzSMLal4UT0L3EU3GRjrOw-kA/view?usp=drive_link"
+            },
+            {
+                "id":10,
+                "title": "Municipio Casacoima, Piramide de población según grupo de edades, 2025",
+                "description": " Proyecciones basadas en el Censo de Población y Vivienda 2011.",
+                "format": "PDF",
+                "url": "https://drive.google.com/file/d/1zt9lDkhjuYV9OLBvlOHtD-0OuiErhzag/view?usp=drive_link"
+            },
+            {
+                "id":11,
+                "title": "Municipio Perdernales, Piramide de población según grupo de edades, 2025",
+                "description": "Proyecciones basadas en el Censo de Población y Vivienda 2011",
+                "format": "PDF",
+                "url": "https://drive.google.com/file/d/12vsgHZgmKu1iXeFbMan0AsZXb5cLzQzF/view?usp=drive_link"
+            },
+            {
+                "id":12,
+                "title": "Municipio Pedernales, Población, según grupos de edad y sexo, 2020-2025",
+                "description": "Proyecciones al 30 de junio de cada año, calculadas en el segundo trimestre del año 2013, con base al Censo 2011.",
+                "format": "PDF",
+                "url": "https://drive.google.com/file/d/1pUXHvPD4P8T0IYsFChlHiLQX2D94Usot/view?usp=drive_link"
+            },
+             {
+                "id":13,
+                "title": "Municipio Tucupita, Piramide de población según grupo de edades, 2025",
+                "description": "Proyecciones basadas en el Censo de Población y Vivienda 2011",
+                "format": "PDF",
+                "url": "https://drive.google.com/file/d/1DO-76i3NdBCzADlXQyTV6gjU8wwHxWNY/view?usp=drive_link"
+            },
+            {
+                "id":14,
+                "title": "Municipio Tucupita, Población, según grupos de edad y sexo, 2020-2025",
+                "description": "Proyecciones al 30 de junio de cada año, calculadas en el segundo trimestre del año 2013, con base al Censo 2011.",
+                "format": "PDF",
+                "url": "https://drive.google.com/file/d/1qdQLsdu7iwE0SMhtZ5wwFXURKTU5Zu4O/view?usp=drive_link"
             },
             
             
