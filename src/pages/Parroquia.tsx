248b79fcb52815e8532ba948a50e35edd0a83df0
fguedez1311 <46085315+fguedez1311@users.noreply.github.com.>
2025-10-17 09:36:05 -0400

    Se agregó la tabla páginada con las comunidades

--- src/pages/Parroquia.tsx
+++ src/pages/Parroquia.tsx
@@ -1,11 +1,15 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
+import { TablaParroquia } from "@/components/TablaParroquia"
+
+import { publicacionesDataParroquia } from "@/data/data_parr"
 
 
 export const Parroquia = () => {
   return (
     <>
-         <CustomJumbotron title="Publicaciones de Estadisticas y catografías de las comunidades" description="En la Tabla se encuentra publicaciones de estádistica y las catorgrafía de las comunidades que pertenece a las diferentes parroquias del estado  en formato PDF,XLS,HTML, y de imágenes"/>
+         <CustomJumbotron title="Publicaciones de Estadisticas y cartografías de las comunidades" description="En la Tabla se encuentra publicaciones de estádistica y las catorgrafía de las comunidades que pertenece a las diferentes parroquias del estado  en formato PDF,XLS,HTML, y de imágenes"/>
          
+          <TablaParroquia data={publicacionesDataParroquia} />
          
          
        </>
